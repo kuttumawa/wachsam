670f6e58d6070dc6324a2bb4da404b2ca9b13160
unknown <dlinares@LES000341.corp.capgemini.com>
2015-03-13 14:09:38 +0100

    ok

---
+++ src/main/java/es/io/wachsam/model/DB.java
@@ -0,0 +1,38 @@
+package es.io.wachsam.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class DB {
+  @Id
+  Long id;
+  Long size;
+
+public DB(){
+	super();
+	}
+public DB(Long i, Long size) {
+	super();
+	this.id = i;
+	this.size = size;
+}
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public Long getSize() {
+	return size;
+}
+
+public void setSize(Long size) {
+	this.size = size;
+}
+}
